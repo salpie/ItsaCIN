f3beb971127b0bd4254a314735c956c825847da3
salpie <senowinski@gmail.com>
2020-04-03 16:34:05 +0100

    added plot for birth death when a cell is mutated

--- test_b_d.jl
+++ test_b_d.jl
@@ -6,7 +6,7 @@ b = Normal(0.5,0.2)
 #death rate distribution
 d = Normal(0.45,0.2)
 
-
+### first run and make birth and death plots
 d_death_probability = rand(d, 1000)
 b_birth_probability = rand(b, 1000)
 
@@ -46,60 +46,13 @@ savefig("Birth_Death_Distribution_detectable_size_2000_.pdf")
 number_deaths = x[10000,:]
 
 
-#death rate distribution
-#μ = Poisson(0.02)
-μ = Gamma(0.02)
-## add a mutation and see how this changes the dynamics
-
-mutable struct CancerCell
-    CN::Int64
-    N::Int64
-    b::Float64
-    d::Float64
-end
-
-function copycell(cancercellold::cancercell)
-    newcancercell::CancerCell = cancercell(copy(cancercellold.mutations), copy(cancercellold.death), copy(cancercellold.epnumber),copy(cancercellold.escaped))
-end
-
-#cancerCell = CancerCell(2, 1, 0.02, 0.01)
-#Tumour=CancerCell[]
-#Tumour[1] = CancerCell(2, 1, 0.02, 0.01)
-
-x = 1 # I want the array to be of length 10
-# create an uninitialized 1D array of size `x`
-Tumour = Array{CancerCell}(undef, x) # or `Vector{Coords}(undef, x)`
-
-# initialize the first tumour cancercell
-for i = 1:x
-    Tumour[i] = CancerCell(2, 1, 0.02, 0.01)
-end
-
-for time in 1:Time
-    i = length(Tumour)
-    for cell in 1:length(Tumour)
-        if (Tumour[cell].b)
-            cancercell_old = Tumour[cell]
-            New_cancercell = copycell(cancercell_old)
-            Tumour = push!(Tumour, New_cancercell)
-        end
-        if (Tumour[cell].d)
-            Tumour = deleteat!(Tumour, cell)
-        end
-        if no change # do nothing
-        end
-
-    end
-    P[time] = length(Tumour)
-end
-
 # copy functions - checkout?
 push!(Tumour, CancerCell)
 for cell in time
     for i in Tumour[cell,time]
 
 
-
+### second run and make birth and death plots - with chr13 gain added at different time points
 ########## new trial with a mutation added
 
 time = 200
